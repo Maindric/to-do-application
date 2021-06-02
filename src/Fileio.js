9a2484d74399046cd348e7c6420155ff9d53c61b
ZachCS50 <63368189+ZachCS50@users.noreply.github.com>
2021-06-02 17:56:39 -0400

    Delete Fileio.js

--- src/Fileio.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect} from "react"
-
-export const SaveJson = (obj) => {
-    useEffect(() => {
-        const json = JSON.stringify(obj);
-        localStorage.setItem("items", json);
-    }, [obj]);
-}
